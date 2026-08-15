1f61ffa896e7c777707e54500ee7d72070512bc4
rain <308736589+despondence@users.noreply.github.com>
2026-08-15 15:39:53 +0800

    pass extra args to qemu for debugging

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ cp ../target/x86_64-unknown-uefi/debug/mist.efi esp/EFI/BOOT/BOOTX64.EFI
 cp /usr/share/qemu/edk2-x86_64-code.fd ovmf.fd
 
 echo "[mist] run in qemu"
-qemu-system-x86_64 -machine q35 -m 16G -smp 4 -drive if=pflash,format=raw,readonly=on,file=ovmf.fd -drive format=raw,file=fat:rw:esp -serial stdio -device isa-debug-exit,iobase=0xf4,iosize=0x04 -no-reboot
+qemu-system-x86_64 -machine q35 -m 16G -smp 4 -drive if=pflash,format=raw,readonly=on,file=ovmf.fd -drive format=raw,file=fat:rw:esp -serial stdio -device isa-debug-exit,iobase=0xf4,iosize=0x04 -no-reboot "$@"
